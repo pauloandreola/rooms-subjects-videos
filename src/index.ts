20914154bf295c3f8accbe1756ae1485f6cb2817
Paulo Andreola <pauloandreola@gmail.com>
2022-12-28 13:19:24 -0300

    feat: index file

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { AppDataSource } from "./data-source";
+
+
+AppDataSource.initialize().then(() => {
+  const app = express();
+
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    return res.json('novo teste')
+  });
+
+  return app.listen(process.env.PORT);
+});
